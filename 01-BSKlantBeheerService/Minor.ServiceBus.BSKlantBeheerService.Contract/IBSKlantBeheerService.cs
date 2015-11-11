26fe2676f04fb9b424d324041622835ce2b0d7d7
esrahofstede <esrahofstede@gmail.com>
2015-11-11 15:57:29 +0100

    service and xsd

---
+++ 01-BSKlantBeheerService/Minor.ServiceBus.BSKlantBeheerService.Contract/IBSKlantBeheerService.cs
@@ -0,0 +1,23 @@
+﻿using minorservicebusbsklantbeheer.v1.message;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Minor.ServiceBus.BSKlantBeheerService.Contract
+{
+    [ServiceContract(Namespace = "urn:minor:servicebus:bsklantbeheer:v1")]
+    public interface IBSKlantBeheerService
+    {
+        [OperationContract]
+        void FindKlanten();
+
+        [OperationContract]
+        FindKlantenResponseMessage FindKlantenBy();
+
+        [OperationContract]
+        void InsertCursusInstanties();
+    }
+}
---
+++ 01-BSKlantBeheerService/Minor.ServiceBus.BSKlantBeheerService.Implementation/BSKlantBeheerServiceHandler.cs
@@ -0,0 +1,29 @@
+﻿using Minor.ServiceBus.BSKlantBeheerService.Contract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Minor.ServiceBus.BSKlantBeheerService.Implementation
+{
+    public class BSKlantBeheerServiceHandler : IBSKlantBeheerService
+    {
+        public void FindKlanten()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void FindKlantenBy()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void InsertCursusInstanties()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+
+}
