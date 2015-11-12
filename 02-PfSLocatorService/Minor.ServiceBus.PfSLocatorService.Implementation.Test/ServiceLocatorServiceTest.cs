8f47be8022fa1c9ad06d164e47780ad1cd74a414
esrahofstede <esrahofstede@gmail.com>
2015-11-12 14:45:26 +0100

    wsdl updates

---
+++ 02-PfSLocatorService/Minor.ServiceBus.PfSLocatorService.Implementation.Test/ServiceLocatorServiceTest.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Minor.ServiceBus.PfSLocatorService.Contract.DTO;
+using Moq;
+using Minor.ServiceBus.PfSLocatorService.DAL;
+using System.ServiceModel;
+
+namespace Minor.ServiceBus.PfSLocatorService.Implementation.Test
+{
+    [TestClass]
+    public class ServiceLocatorServiceTest
+    {
+        [TestMethod]
+        [ExpectedException(typeof(FaultException<ServiceLocationServiceFault>))]
+        public void FindMetadataEndpointAddress_NoName()
+        {
+            // Arrange
+            string name = null;
+            string profile = "Development";
+            decimal? version = 1.0m;
+            ServiceLocation serviceLocation = new ServiceLocation
+            {
+                Name = name,
+                Profile = profile,
+                Version = version
+            };
+
+            var mock = new Mock<IServiceLocationDataMapper>(MockBehavior.Strict);
+            
+            var target = new ServiceLocatorService(mock.Object);
+
+            // Act
+            var result = target.FindMetadataEndpointAddress(serviceLocation);
+
+            // Assert
+            //Exception thrown
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(FaultException<ServiceLocationServiceFault>))]
+        public void FindMetadataEndpointAddress_NoProfile()
+        {
+            // Arrange
+            string name = "BSKlantBeheer";
+            string profile = null;
+            decimal? version = 1.0m;
+            ServiceLocation serviceLocation = new ServiceLocation
+            {
+                Name = name,
+                Profile = profile,
+                Version = version
+            };
+
+            var mock = new Mock<IServiceLocationDataMapper>(MockBehavior.Strict);
+
+            var target = new ServiceLocatorService(mock.Object);
+
+            // Act
+            var result = target.FindMetadataEndpointAddress(serviceLocation);
+
+            // Assert
+            //Exception thrown
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(FaultException<ServiceLocationServiceFault>))]
+        public void FindMetadataEndpointAddress_EmptyName()
+        {
+            // Arrange
+            string name = "";
+            string profile = "Development";
+            decimal? version = 1.0m;
+            ServiceLocation serviceLocation = new ServiceLocation
+            {
+                Name = name,
+                Profile = profile,
+                Version = version
+            };
+
+            var mock = new Mock<IServiceLocationDataMapper>(MockBehavior.Strict);
+
+            var target = new ServiceLocatorService(mock.Object);
+
+            // Act
+            var result = target.FindMetadataEndpointAddress(serviceLocation);
+
+            // Assert
+            //Exception thrown
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(FaultException<ServiceLocationServiceFault>))]
+        public void FindMetadataEndpointAddress_EmptyProfile()
+        {
+            // Arrange
+            string name = "BSBeheerService";
+            string profile = "";
+            decimal? version = 1.0m;
+            ServiceLocation serviceLocation = new ServiceLocation
+            {
+                Name = name,
+                Profile = profile,
+                Version = version
+            };
+
+            var mock = new Mock<IServiceLocationDataMapper>(MockBehavior.Strict);
+
+            var target = new ServiceLocatorService(mock.Object);
+
+            // Act
+            var result = target.FindMetadataEndpointAddress(serviceLocation);
+
+            // Assert
+            //Exception thrown
+        }
+    }
+}
