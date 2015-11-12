0e8707ac4540af946dafd5e1e4ad802130661363
esrahofstede <esrahofstede@gmail.com>
2015-11-12 11:51:27 +0100

    split PfSLocatorService

---
+++ 02-PfSLocatorService/Minor.ServiceBus.PfSLocatorService.DAL/ServiceLocationXMLDataMapper.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Minor.ServiceBus.PfSLocatorService.DAL
+{
+    public class ServiceLocationXMLDataMapper : IServiceLocationDataMapper
+    {
+        public string FindMetadataEndpointAddress(string name, string profile)
+        {
+            return "http://localhost:30412/BSKlantbeheer/mex";
+        }
+
+        public string FindMetadataEndpointAddress(string name, string profile, decimal version)
+        {
+            return "http://localhost:30412/BSKlantbeheer/mex";
+        }
+    }
+}
