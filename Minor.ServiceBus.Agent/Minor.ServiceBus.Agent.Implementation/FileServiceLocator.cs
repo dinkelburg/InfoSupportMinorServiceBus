dd8e2a2be6a2a6af95585a4181681bd4fd075863
Levi Steenbergen <lm.steenbergen@student.han.nl>
2015-11-12 12:46:27 +0100

    FileService afgemaakt

--- Minor.ServiceBus.Agent/Minor.ServiceBus.Agent.Implementation/FileServiceLocator.cs
+++ Minor.ServiceBus.Agent/Minor.ServiceBus.Agent.Implementation/FileServiceLocator.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,28 +10,59 @@ namespace Minor.ServiceBus.Agent.Implementation
 {
     public class FileServiceLocator : IServiceLocator
     {
+        private string _filePath;
+
+        public FileServiceLocator(string filePath)
+        {
+            if (!string.IsNullOrEmpty(filePath))
+            {
+                _filePath = filePath;
+            } else
+            {
+                throw new FilePathNotDefinedException("File path is not defined.");
+            }
+        }
 
         public string FindMetadataEndpointAddress(string name, string profile)
         {
-            //load xml file in
-            //check with name, profile and version
-            //return metadatAddress
-
-            var locationdata = new locationData();
-
-            XmlSerializer s = new XmlSerializer(typeof(locationData));
-            locationData result = s.Deserialize(stream) as personen;
-
-            throw new NotImplementedException();
+            return GetMetaDataEndPointAdress(name, profile);
         }
 
         public string FindMetadataEndpointAddress(string name, string profile, decimal version)
         {
-            //load xml file in
-            //check with name, profile and version
-            //return metadatAddress
-
-            throw new NotImplementedException();
+            return GetMetaDataEndPointAdress(name, profile, version);
         }
+
+        public string GetMetaDataEndPointAdress(string name, string profile, decimal? version = null)
+        {
+            var data = LoadXMLFile<locationData>();
+            foreach (var servicelocation in data.serviceLocation)
+            {
+                if(version != null)
+                {
+                    if (servicelocation.name == name 
+                        && servicelocation.profile == profile
+                        && servicelocation.version == version)
+                    {
+                        return servicelocation.metadataAddress;
+                    }
+                }
+                if (servicelocation.name == name && servicelocation.profile == profile)
+                {
+                    return servicelocation.metadataAddress;
+                }
+            }
+            throw new ServiceLocationDoesntExistsException("The given service location doesn't exists inside of the given xml file.");
+        }
+
+        public T LoadXMLFile<T>() {
+            XmlSerializer serializer = new XmlSerializer(typeof(locationData));
+            using (StreamReader reader = new StreamReader(_filePath))
+            {
+                var data = serializer.Deserialize(reader);
+                return (T)data;
+            }
+        }
+
     }
 }
